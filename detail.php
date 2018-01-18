044661ace365aa39ddcac195ec4cbb968c8454fb
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-01-19 00:13:55 +0700

    ลอง

--- detail.php
+++ detail.php
@@ -102,20 +102,26 @@
 
 <div class="container">  
 	 <div class="row">
-	 	<div class="col-12" >
-	 		<?php 
+	 	<?php 
+	 		$i
+	 		for($i=0;$i<$count_room;$i++){
+	 			if($i==0||$i==$count_room-1){
+	 				echo'<div class="col-12" >'.
+	 			}
+	 			else{
+	 				$size_room = 12/$count_room-2
+	 				echo'<div class="col-'.$size_room.'" >'.
+	 			}
+	 				
+	 			.'<div class="panel panel-default">
+					<div class="panel-body"><img src="img/light-open.png" class="img-responsive" style="width:100%" alt="Image"></div>
+				</div>
 
-	 			echo'	
-	 			<div class="panel panel-default">
-					
-				</div>';
-
-
-
-
-	 		?>
 	 	
-	 	</div>
+	 		</div>';
+	 		}
+	 		
+	 	?>
 	 </div>
 
 
