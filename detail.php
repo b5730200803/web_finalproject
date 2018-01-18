a31934aca5f5f2e4fe6420882938ad52f0d26260
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-01-18 23:14:51 +0700

    ลอง

--- detail.php
+++ detail.php
@@ -107,7 +107,7 @@
 <div class="container">  
 	 <div class="row">
 	 	<div class="col-12" class="front-back" >
-	 		lksdjfglkdfgljghlkdjghlkjdlgkjhlkdghkldglkhjgfhlks
+	 		lksdjfglkdfgljgh
 	 	</div>
 	 </div>
 
