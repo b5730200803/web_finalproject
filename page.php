f4b38b2aec733b6ffd8821f50d992fb7a59e3918
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-02-09 02:23:43 +0700

    dsdf

--- page.php
+++ page.php
@@ -9,7 +9,10 @@
 <head>
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+  <link href="./Non-responsive Template for Bootstrap_files/non-responsive.css" rel="stylesheet">
+  <link href="./Non-responsive Template for Bootstrap_files/non-responsive.css" rel="stylesheet">
+
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
