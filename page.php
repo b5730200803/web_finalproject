7748e6625e1b9bd5a868c246fdeabed3b5a8cb79
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-02-09 03:20:35 +0700

    ย่อขยาย

--- page.php
+++ page.php
@@ -15,7 +15,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <style>
-  	@media only screen and (max-width: 500px) {
+  	@media only screen and (max-width: 1200px) {
   		#ImageLogo{
   			width: 50%;
   			height: auto;
