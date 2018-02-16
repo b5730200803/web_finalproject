a2f2e10c4a664f251b83632474adb38e9625eb3b
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-02-17 01:26:34 +0700

    ลอง

--- page.php
+++ page.php
@@ -58,18 +58,15 @@
 
 <div class="jumbotron" >
 
-	<div class="container ">
+	
 		<div class="row">
 			
-			<div class="col-lg-4" style="padding:20px;">
-				<img id="ImageLogo" src="img/EN-limites-chatbot.png" width="120%" height="auto">
-			</div>
 			<div id="textHead" class="col-lg-8" >
 				<fontHead>การควบคุมการเปิดปิดไฟผ่านทางไลน์แชทบอท</fontHead>
 				<p>Remote Lighting Control via LINE Chatbot</p>
 			</div>
 		</div>
-	</div>
+	
 </div>
 
 
