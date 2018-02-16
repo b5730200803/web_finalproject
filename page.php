2c5841cea4a114352ac03c44e70db6d7a1c63d23
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-02-17 05:00:59 +0700

    ไำ

--- page.php
+++ page.php
@@ -69,13 +69,11 @@
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
+                               
       </button>
      
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
-      
       <form class="navbar-form navbar-right" role="search">
         <div class="form-group input-group">
           <input type="text" class="form-control" placeholder="Search..">
