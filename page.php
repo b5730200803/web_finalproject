ed521bbb611894c511657bf337baf1dcdee63cc0
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-02-17 04:10:25 +0700

    rgt

--- page.php
+++ page.php
@@ -40,6 +40,9 @@
     	padding-bottom: 10px; 
     	padding-left:  100px;
     }
+    p{
+    	color:  #FFFFFF;
+    }
    
 
   	@media only screen and (max-width: 900px) {
@@ -60,6 +63,42 @@
    
 </head>
 <body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Messages</a></li>
+      </ul>
+      <form class="navbar-form navbar-right" role="search">
+        <div class="form-group input-group">
+          <input type="text" class="form-control" placeholder="Search..">
+          <span class="input-group-btn">
+            <button class="btn btn-default" type="button">
+              <span class="glyphicon glyphicon-search"></span>
+            </button>
+          </span>        
+        </div>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> My Account</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+	
 
 <div class="jumbotron" >
 		<div class="row">
