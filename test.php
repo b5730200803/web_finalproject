2c750ba3ffa3333de100a0434ea166077c8b3fad
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-02-18 04:07:11 +0700

    แก้

--- test.php
+++ test.php
@@ -53,7 +53,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">Logo</a>
+      น้องตะวัน
     </div>
     <div class="collapse navbar-collapse" id="Search">
             <?php if(!empty($query)){
