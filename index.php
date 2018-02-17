99f055a169a944b01048461af2e5117368b212ae
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-02-17 16:28:06 +0700

    asfd

--- index.php
+++ index.php
@@ -5,15 +5,9 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-   
-
-    <title>Heroic Features - Start Bootstrap Template</title>
-
-    <!-- Bootstrap core CSS -->
+    <title>Web-LineChatBot</title>
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/heroic-features.css" rel="stylesheet">
+    <link href="https://raw.githubusercontent.com/BlackrockDigital/startbootstrap-heroic-features/master/css/heroic-features.css" rel="stylesheet">
 
   </head>
 
