d6e042fe0e2e1291cf046a745b9415063198f05e
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-02-17 19:40:45 +0700

    oiui

--- detail.php
+++ detail.php
@@ -143,9 +143,7 @@
 									echo "<th><img src='".$mlab_userdetail->pictureUrl."' width='70x' style='border-radius:100%' /></th>";
 								}
 
-								
-
-
+						
 								echo '</tr>
 
 
@@ -213,19 +211,6 @@
 			
 		</div>	
 
-
-		     <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card">
-            <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-            </div>
-            <div class="card-footer">
-              <a href="#" class="btn btn-primary">Find Out More!</a>
-            </div>
-          </div>
-        </div>
 	</div>
 </div>
 
