b32c79debd498a97ca6b289319db982c4010f336
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-02-18 06:59:53 +0700

    ลอง

--- test2.php
+++ test2.php
@@ -23,15 +23,14 @@
         <div class="container">
           <div class="row">
             <div class="col-sm-8 col-md-7 py-4">
-              <h4 class="text-white">About</h4>
-              <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+              <h4 class="text-white">เกี่ยวกับ</h4>
+              <p class="text-muted">เว็บไซต์นนี้เป็นแบบจำลองการส่งข้อมูลจากไลน์ มาแสดงผลการควบคุมเปืดปิดไฟผ่านทางแชทบอท</p>
             </div>
             <div class="col-sm-4 offset-md-1 py-4">
-              <h4 class="text-white">Contact</h4>
+              <h4 class="text-white">จัดทำโดย</h4>
               <ul class="list-unstyled">
-                <li><a href="https://getbootstrap.com/docs/4.0/examples/album/#" class="text-white">Follow on Twitter</a></li>
-                <li><a href="https://getbootstrap.com/docs/4.0/examples/album/#" class="text-white">Like on Facebook</a></li>
-                <li><a href="https://getbootstrap.com/docs/4.0/examples/album/#" class="text-white">Email me</a></li>
+                <li><font color="white">นางสาว ศุภากร ติระเดชยวัฒน์</font></li>
+                <li><font color="white">นาย สหชัย ยวงพานิช</font></li>
               </ul>
             </div>
           </div>
@@ -39,9 +38,9 @@
       </div>
       <div class="navbar navbar-dark bg-dark box-shadow">
         <div class="container d-flex justify-content-between">
-          <a href="https://getbootstrap.com/docs/4.0/examples/album/#" class="navbar-brand d-flex align-items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-            <strong>Remote Lighting Control via LINE Chatbot</strong>
+          <a href="#" class="navbar-brand d-flex align-items-center">
+             <span class="glyphicon glyphicon-home"></span>
+            <strong><font color="#32CD32">น้องตะวัน</font></strong>
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -52,20 +51,6 @@
 
     <main role="main">
 
-      <section class="jumbotron text-center">
-        <div class="container">
-          <h1 class="jumbotron-heading">น้องตะวัน</h1>
-          <p class="lead text-muted">การควบคุมการเปิดปิดไฟผ่านทางไลน์แชทบอท</p>
-          <p>
-            <form class="navbar-form navbar-right" action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="GET">
-              <div class="form-group">
-                <input type="text" size="35" class="form-control text-center" placeholder="กรอกชื่อบ้านหรือรหัสบ้าน 6 ตัว" name="q" >
-              </div>
-              <button type="submit" class="btn btn-default" width="100%">ค้นหา</button>
-            </form>
-          </p>
-        </div>
-      </section>
 
       <div class="album py-5 bg-light">
         <div class="container">
