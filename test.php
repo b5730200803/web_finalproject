c7de3b8bba136f99baa863f47d42b8f7b22c2cf9
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-02-18 06:15:49 +0700

    แก้

--- test.php
+++ test.php
@@ -49,11 +49,10 @@
       </div>
       <div class="navbar navbar-dark bg-dark box-shadow">
         <div class="container d-flex justify-content-between">
-          <a href="https://getbootstrap.com/docs/4.0/examples/album/#" class="navbar-brand d-flex align-items-center">
+   
             <img src="img/icons8-chat-40">
             <strong>Remote Lighting Control via LINE Chatbot</strong>
-          </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
         </div>
