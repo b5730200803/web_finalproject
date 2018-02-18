5a21ff5c2c987f7add082bfda0b8748de09da89b
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-02-18 17:18:28 +0700

    ลอง

--- test2.php
+++ test2.php
@@ -99,22 +99,6 @@
 
     <div class="container">    
       <div class="row">
-
-
-
-        <div class="card">
-    <div class="card-body">
-      <h4 class="card-title">Card title</h4>
-      <p class="card-text">Some example text. Some example text.</p>
-      <a href="#" class="card-link">Card link</a>
-      <a href="#" class="card-link">Another link</a>
-    </div>
-  </div>
-
-
-
-
-     
         <?php
           $count_room = count($mlab_data[0]->source);
           echo '
