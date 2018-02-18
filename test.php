71c78f28547077dc478b6dae2b21ea51ebbb8d14
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-02-18 07:28:32 +0700

    แก้

--- test.php
+++ test.php
@@ -49,7 +49,6 @@
       <div class="navbar navbar-dark bg-dark box-shadow">
         <div class="container d-flex justify-content-between">
           <a href="#" class="navbar-brand d-flex align-items-center">
-             <span class="glyphicon glyphicon-home"></span>
             <strong><font color="#32CD32">น้องตะวัน</font></strong>
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
