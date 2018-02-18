bd6e9809a8513f5a56740909daefca95b3fe4062
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-02-18 16:43:38 +0700

    ลอง

--- test2.php
+++ test2.php
@@ -103,10 +103,9 @@
         <?php
           $count_room = count($mlab_data[0]->source);
           echo '
-          <div class="col-sm-12">
-            <div class="panel panel-success">
-              <div class="panel-heading ">รายละเอียด </div>
-              <div class="panel-body">
+          <div class="col-md-12">
+            <div class="card mb-4 box-shadow">
+              <div class="card body">
                 <b>หมายเลขบ้าน</b> : '.$mlab_data[0]->id.' <br>
                 <b>ชื่อบ้าน</b> : '.$mlab_data[0]->name.'<br>
                 <b>รหัสบ้าน</b> : '.$mlab_data[0]->password.'<br>
