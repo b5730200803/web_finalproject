c7b6d1ab702713a56e662a7b21d44b49a4fca852
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-05-02 15:08:25 +0700

    css

--- test.php
+++ test.php
@@ -35,7 +35,7 @@
     <title>Web Line ChatBot</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="https://getbootstrap.com/docs/4.0/examples/album/album.css" rel="stylesheet">
