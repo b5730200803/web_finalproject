68313eca7896af5bbc7ed1e441862cbef7e996bf
b5730200803 <33450058+b5730200803@users.noreply.github.com>
2018-05-15 02:36:19 +0700

    เพิ่มลิงค์

--- test2.php
+++ test2.php
@@ -92,7 +92,7 @@
       </div>
       <div class="navbar navbar-dark bg-dark box-shadow">
         <div class="container d-flex justify-content-between">
-          <a href="#" class="navbar-brand d-flex align-items-center">
+          <a href="test.php" class="navbar-brand d-flex align-items-center">
             <strong><font color="#32CD32">น้องตะวัน</font></strong>
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
